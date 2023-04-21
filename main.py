114cb92eb325a2991b7c0bdac453e4b9cbc5a2d9
Velvv <reymondd7@gmail.com>
2023-04-21 22:24:39 +0200

    Version2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from tkinter import *
 from pyStandardGalacticAlphabet import SGA
+import pyglet, os
 
 sga = SGA()
 
 label = None
 window = tk.Tk()
 window.title("Enchantment Translator")
-window.geometry("1800x300")
+window.geometry("800x300")
 window.resizable(height=False, width=False)
 window['bg'] = '#c976fc'
 
@@ -18,7 +19,7 @@ def printValue():
     en_to_sga = sga.encode(string)
     if label != None:
         remove_label()
-    label = Label(window, text=f'{en_to_sga}', pady=20, bg='#c976fc', font= ("Arial", 25))
+    label = tk.Label(window, text=f'{en_to_sga}', pady=20, bg='#c976fc', wraplength=800, font= ("Arial", 25))
     label.pack()
 
 def remove_label():
